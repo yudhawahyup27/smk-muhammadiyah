f31aec4a73bcf17314b7d1639898c82b9808446b
yudhawahyup27 <wyudha104@gmail.com>
2025-09-11 04:43:21 +0000

    update di hosting

--- index.php
+++ index.php
@@ -10,11 +10,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+
+require __DIR__.'/vendor/autoload.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
+
 
 $app->handleRequest(Request::capture());
